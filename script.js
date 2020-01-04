d75a9ac7b7ac8b008a8aa6ff978de5be88ebd9c2
Alexanne BRUNOLD <alexannebrunold@MacBook-Pro-de-Alexanne.local>
2020-01-04 15:09:32 +0100

    html css et appel functions js

---
+++ script.js
@@ -0,0 +1,24 @@
+const app = () => {
+  const song = document.querySelector('.song');
+  const play = document.querySelector('.play');
+  const outline = document.querySelector('.moving-outline circle');
+  const video = document.querySelector('.vid-container video');
+
+
+  //Sounds
+  const sounds = document.querySelectorAll('.sound-picker button');
+
+  //Time Display
+  const timeDisplay = document.querySelector('.time-display');
+
+  //Get the Lenggth of the outline
+  const outlineLength = outline.getTotalLength();
+  console.log(outlineLength);
+
+  //Duration
+  let FakeDuration = 600;
+
+  outline.getElementsByClassName.strokeDasharray = 100;
+};
+
+app();
