d1821924c4a33fe7e3c6714bae1aa5ed49116f69
Alexanne BRUNOLD <alexannebrunold@MacBook-Pro-de-Alexanne.local>
2020-01-05 18:31:55 +0100

    Circle, time

--- script.js
+++ script.js
@@ -10,15 +10,56 @@ const app = () => {
 
   //Time Display
   const timeDisplay = document.querySelector('.time-display');
+  const timeSelect = document.querySelector('.time-select');
 
   //Get the Lenggth of the outline
   const outlineLength = outline.getTotalLength();
-  console.log(outlineLength);
+  //console.log(outlineLength);
 
   //Duration
-  let FakeDuration = 600;
+  let fakeDuration = 600;
 
   outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //Play sound
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  //Select sound
+  timeSelect.array.forEach(element => {
+
+  });
+
+  //Create a function specific to stop and play the sounds
+  const checkPlaying = song => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = 'Assets/svg/pause.svg';
+    }
+    else {
+      song.pause();
+      video.pause();
+      play.src = 'Assets/svg/play.svg';
+    }
+  };
+
+  //We can animate the circle
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    //Animate the circle
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+    //Animate the text
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+  };
 };
 
 app();
